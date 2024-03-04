e254d7cc7085130832840b215ca413c39f9f8da5
BAESEONGHUN <129490755+baehoonbae@users.noreply.github.com>
2024-03-04 16:19:14 +0900

    [Add] editPageFunc.js

---
+++ post-project/src/main/resources/static/js/editPageFunc.js
@@ -0,0 +1,25 @@
+    $(document).ready(function(){
+       $('#editForm').submit(function(event){
+           var postId = $('#editForm').attr('action').split('/')[2];
+           var newTitle = $('#title').val();
+           var newTexts = $('#content').val();
+
+           $.ajax({
+               url: '/post/' + postId + '/edit',
+               type: 'POST',
+               data: {
+                   id: postId,
+                   title: newTitle,
+                   content: newTexts
+               },
+               success: function(response) {
+                   alert('게시물이 성공적으로 수정되었습니다.');
+                   window.location.href = '/post/' + postId;
+               },
+               error: function(xhr, status, error) {
+                   alert('서버 오류가 발생했습니다.');
+               }
+           });
+           event.preventDefault(); // 기본 폼 제출 동작 중지
+       });
+   });
