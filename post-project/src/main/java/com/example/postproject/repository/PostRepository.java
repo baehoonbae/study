2efcb31953616dc9e07f96b19b0c6b9a12e1906e
BAESEONGHUN <129490755+baehoonbae@users.noreply.github.com>
2024-03-04 16:14:29 +0900

    [Update] PostRepository.java
    - 스프링 데이터 JPA 추가에 따른 수정

--- post-project/src/main/java/com/example/postproject/repository/PostRepository.java
+++ post-project/src/main/java/com/example/postproject/repository/PostRepository.java
@@ -1,33 +1,11 @@
 package com.example.postproject.repository;
 
 import com.example.postproject.domain.Post;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.*;
+import java.util.List;
 
-@Repository
-public class PostRepository {
-    private static Map<Long, Post> posts = new HashMap<>();
-    private static long sequence = 0;
+public interface PostRepository extends JpaRepository<Post, Long> {
 
-    public void save(Post post) {
-        post.setId(++sequence);
-        posts.put(post.getId(), post);
-    }
-
-    public void update(long id, Post post) {
-        posts.put(id, post);
-    }
-
-    public List<Post> findAll() {
-        return new ArrayList<>(posts.values());
-    }
-
-    public Post findById(long id) {
-        return posts.get(id);
-    }
-
-    public void delete(long id) {
-        posts.remove(id);
-    }
+    List<Post> findAllByOrderByIdDesc();
 }
