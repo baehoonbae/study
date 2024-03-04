7292646db76c9b66a6c9edb86cb8ad607b4e820b
BAESEONGHUN <129490755+baehoonbae@users.noreply.github.com>
2024-03-04 16:13:33 +0900

    [Update] Post.java
    - MySQL 데이터베이스 "posts" 테이블 매핑

--- post-project/src/main/java/com/example/postproject/domain/Post.java
+++ post-project/src/main/java/com/example/postproject/domain/Post.java
@@ -1,17 +1,31 @@
 package com.example.postproject.domain;
 
+import jakarta.persistence.*;
 import lombok.*;
 
+@Entity
+@Table(name="posts")
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(name = "title")
     private String title;
+
+    @Column(name = "content")
     private String content;
 
+    @Column(name = "writer")
     private String writer;
+
+    @Column(name = "password")
     private String password;
 
+    @Column(name = "updateDate")
     private String updateDate;
 }
